56879ab1f77239e5e2e69af5ab851f45f5cb2b5b
Omer <omersrrol@gmail.com>
2021-11-12 13:47:43 +0200

    Suppports medial line bending

--- ImageProcessing1.py
+++ ImageProcessing1.py
@@ -1,6 +1,7 @@
 import cv2
 import os
-#gyghsaqeafwedadasd
+import numpy as np
+import matplotlib.pyplot as plt
 # path = r'C:\Users\Omer\Desktop\birb.png'
 root_path = os.path.dirname(os.path.abspath(__file__))
 path = cv2.imread(os.path.join(root_path, "index.jpeg"))
@@ -8,14 +9,18 @@ img = path
 # variables
 ix = -1
 iy = -1
+jx = 0
+jy = 0
+px=-1
+py = -1
+
 drawing = False
 d = False
-one = 0
-two = 0
+p= True
 
 
 def draw_reactangle_with_drag(event, x, y, flags, param):
-    global ix, iy, drawing, img, d, one, two
+    global ix, iy, drawing, img, d, jx, jy
     if event == cv2.EVENT_LBUTTONDOWN:
         drawing = True
         ix = x
@@ -35,12 +40,23 @@ def draw_reactangle_with_drag(event, x, y, flags, param):
         cv2.rectangle(img2, pt1=(ix, iy), pt2=(x, y), color=(0, 255, 0), thickness=2)
         img = img2
         d = True
-        one = x
-        two = y
+        jx = x
+        jy = y
+
+def draw_curve(event, x, y, flags, param):
+    global p,px,py
+    if event == cv2.EVENT_LBUTTONDOWN:
+        p = False
+        px = x
+        py = y
+
+
+
 
 
 cv2.namedWindow(winname="Title of Popup Window")
 cv2.setMouseCallback("Title of Popup Window", draw_reactangle_with_drag)
+
 
 while True:
     cv2.imshow("Title of Popup Window", img)
@@ -49,12 +65,31 @@ while True:
         cv2.destroyAllWindows()
     if d:
         cv2.imshow("Title of Popup Window", img)
-        median = round((ix + one) / 2)
+        median = round((ix + jx) / 2)
+        median2 = round ((iy + jy)/2)
         pointA = (median, iy)
-        pointB = (median, two)
+        pointB = (median, jy)
+        cache = img.copy()
         cv2.line(img, pointA, pointB, (0, 0, 255), thickness=2)
+        cv2.imshow("Title of Popup Window", img)
+        cv2.setMouseCallback("Title of Popup Window", draw_curve)
+        while p:
+            if cv2.waitKey(10) == 27:
+                break
+                cv2.destroyAllWindows()
+
+        #medial line bending
+        img = cache
+        axisa = abs(median - px)
+        axisb = round(abs(iy-jy)/2)
+        if (median>px):
+            cv2.ellipse(img, (median, median2),(axisa,axisb),0,90,270,(0,0,255),2 )
+        else:
+            cv2.ellipse(img, (median, median2), (axisa, axisb), 180, 90, 270, (0, 0, 255), 2)
+        # Displaying the image
         cv2.imshow("Title of Popup Window", img)
         cv2.waitKey(0)
         break
 
 cv2.destroyAllWindows()
+
